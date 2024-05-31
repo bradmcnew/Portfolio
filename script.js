05b52cd5376383af0a68a6db67263b40943f940d
Brad McNew <mcnewbrad@gmail.com>
2024-05-31 12:17:31 -0400

    Fix scroll feat

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//rotate Icon on hover
 let rotate = (event) => {
     let element = event.target;
     element.style.transform = `rotate(7deg)`;
@@ -10,4 +11,19 @@ let icons = document.getElementsByClassName('icon')
 for (let i = 0; i < icons.length; i++) {
     icons[i].addEventListener('mouseover', rotate);
     icons[i].addEventListener('mouseout', rotateBack);
+}
+
+let highlight = (event) => {
+    let element = event.target;
+    element.style.color = 'rgb(230 ,233 ,241)';
+}
+let unhighlight = (event) => {
+    let element = event.target;
+    element.style.color = 'rgb(155, 165, 185)';
+}
+
+let navItems = document.getElementsByClassName('nav-list');
+for (let i of navItems) {
+    i.addEventListener('mouseover', highlight);
+    i.addEventListener('mouseout', unhighlight);
 }
